d092d61c3113aeeba477a1003a3a873940a08600
dzvinka123 <116108850+dzvinka123@users.noreply.github.com>
2023-12-27 02:23:26 +0200

    Update BirthdayMailStrategy.java

--- lab11/lab11/src/main/java/ucu/edu/ua/strategy/BirthdayMailStrategy.java
+++ lab11/lab11/src/main/java/ucu/edu/ua/strategy/BirthdayMailStrategy.java
@@ -2,7 +2,7 @@ package ucu.edu.ua.strategy;
 
 public class BirthdayMailStrategy implements MailStrategy{
     @Override
-    public String generateMailContent(MailInfo mailInfo) {
+    public String generateMailContent(MailInfo mailInfo){
         return "Dear " + mailInfo.getClient().getName() + ", enjoy our exclusive gift!";
     }
 }
