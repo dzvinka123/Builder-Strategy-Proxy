781b5d4b07ff5844794814f8ce839fe7a6deaf0d
dzvinka123 <116108850+dzvinka123@users.noreply.github.com>
2023-12-27 02:26:12 +0200

    Update MailSender.java

--- lab11/lab11/src/main/java/ucu/edu/ua/strategy/MailSender.java
+++ lab11/lab11/src/main/java/ucu/edu/ua/strategy/MailSender.java
@@ -11,12 +11,12 @@ import com.mailjet.client.resource.Emailv31;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class MailSender {
+public class MailSender{
     public static final String API_KEY = System.getenv("MAILJET_API_KEY");
     public static final String API_SECRET = System.getenv("MAILJET_SECRET_KEY");
     private Map<MailCode, MailStrategy> contentGeneratorMap;
 
-    public MailSender() {
+    public MailSender(){
         contentGeneratorMap = new HashMap<>();
         contentGeneratorMap.put(MailCode.BIRTHDAYMAIL, new BirthdayMailStrategy());
         contentGeneratorMap.put(MailCode.GIFTMAIL, new GiftMailStrategy());
