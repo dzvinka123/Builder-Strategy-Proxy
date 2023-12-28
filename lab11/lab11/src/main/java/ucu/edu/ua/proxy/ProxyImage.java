20110ad2330c29a8b09209a35d60c98baa6e9e84
dzvinka123 <116108850+dzvinka123@users.noreply.github.com>
2023-12-28 12:55:02 +0200

    Update ProxyImage.java

--- lab11/lab11/src/main/java/ucu/edu/ua/proxy/ProxyImage.java
+++ lab11/lab11/src/main/java/ucu/edu/ua/proxy/ProxyImage.java
@@ -1,20 +1,21 @@
 package ucu.edu.ua.proxy;
-
-import lombok.Builder;
 import lombok.Getter;
 
 
-@Builder @Getter
+@Getter
 public class ProxyImage implements MyImage {
     private String filename;
+    private RealImage image = null;
 
     public ProxyImage(String filename) {
         this.filename = filename;
     }
 
     @Override
-    public void displayPhoto() {
-       RealImage image = new RealImage(filename);
+    public void displayPhoto(){
+        if (image == null){
+            image = new RealImage(filename);
+        }
         image.displayPhoto();
     }
 
